0ca8a6d86f7b2d50eb3b807f6cf207980efa81ec
Sarah F <sarahelayna@gmail.com>
2018-01-15 04:46:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
   return array;
 }
+
+function removeElementFromBeginningOfArray(array) {
+  array2 = array.slice(1);
+  return array2;
+}
