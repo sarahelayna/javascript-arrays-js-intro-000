26c0c3d710fbacfb3368c9d427f420bf7bce46f7
Sarah F <sarahelayna@gmail.com>
2018-01-15 04:32:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,11 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 array.unshift(element);
 return array;
 }
+
+function addElementToEndOfArray() {
+
+}
+
+function destructivelyAddElementToEndOfArray() {
+
+}
